9b759ea4d6fdffd03110295ffc626e331af13e5b
Levente Bajczi <bajczilevi@gmail.com>
2025-11-26 16:14:55 +0100

    added license headers

--- .github/scripts/add-license-header.py
+++ .github/scripts/add-license-header.py
@@ -1,4 +1,19 @@
 #!/usr/bin/env python3
+#
+# Copyright 2025 Budapest University of Technology and Economics
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 """
 Script to add license headers to source files.
 Usage: python add-license-header.py <license_file> [directory]
@@ -18,7 +33,7 @@ COMMENT_STYLES = {
     'c': ('/*', ' * ', ' */'),
     'js': ('/*', ' * ', ' */'),
     'jsx': ('/*', ' * ', ' */'),
-    'mlir': ('/*', ' * ', ' */'),
+    'mlir': ('//', '//', ' //'),
     
     # Hash-style comments
     'py': ('#', '# ', '#'),
