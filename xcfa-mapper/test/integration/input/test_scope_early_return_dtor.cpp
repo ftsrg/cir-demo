7e6e0df7a75cafb3cf56d7f85e763df2d3447bc5
Levente Bajczi <bajczilevi@gmail.com>
2026-06-05 12:59:58 +0200

    Added license headers to file

--- xcfa-mapper/test/integration/input/test_scope_early_return_dtor.cpp
+++ xcfa-mapper/test/integration/input/test_scope_early_return_dtor.cpp
@@ -1,3 +1,19 @@
+/*
+ * Copyright 2025 Budapest University of Technology and Economics
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 // cir.scope early-return case: a function-scoped RAII object with an early
 // `return` inside the cleanup region. The dtor must run on BOTH return paths;
 // in CIR the cir.return ops sit inside the cir.cleanup.scope body, so the
--- xcfa-mapper/test/integration/input/test_scope_stmt_expr.cpp
+++ xcfa-mapper/test/integration/input/test_scope_stmt_expr.cpp
@@ -1,3 +1,19 @@
+/*
+ * Copyright 2025 Budapest University of Technology and Economics
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 // cir.scope value-yielding case: GNU statement expression ({ ...; expr; }).
 // The cir.scope yields a value via `cir.yield %v`; the mapper must propagate
 // that value to the scope's SSA result.
